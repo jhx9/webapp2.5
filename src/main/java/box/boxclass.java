510f1bbefc654e62b4cd40d107fd1c4ce33d5db1
jhx9 <jimmy.jhhuang@mic.com.tw>
2016-05-16 15:57:03 +0800

    Update boxclass.java

--- src/main/java/box/boxclass.java
+++ src/main/java/box/boxclass.java
@@ -6,11 +6,12 @@ package box;
  */
 public class boxclass {
         public static void main(String[] args){
-	String a = "1";
-	String b = "";
-	System.out.println(concatenate (a, b));
+        	String a = "1";
+        	String b = "";
+        	System.out.println(concatenate (a, b));
 }
     public static String concatenate(String one, String two) {
     return one + " " + two;
-	}
+    	
+    }
 }
