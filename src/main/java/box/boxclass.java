5c94c6317795ab4a9bfee98a146962f98fb39e10
jhx9 <jimmy.jhhuang@mic.com.tw>
2016-05-16 15:07:26 +0800

    Update boxclass.java

--- src/main/java/box/boxclass.java
+++ src/main/java/box/boxclass.java
@@ -1,12 +1,16 @@
 package box;
-
-public class boxclass{
+/** Jenkins web test class, user input firstname and lastname on webpage 
+ *
+ * @author Jimmy Huang
+ * @version 6.0z Build 9000 2016/05/24
+ */
+public class boxclass {
         public static void main(String[] args){
-	String a="1";
-	String b="";
+	String a = "1";
+	String b = "";
 	System.out.println(concatenate (a, b));
 }
-    public static String concatenate(String one, String two){
+    public static String concatenate(String one, String two) {
     return one + " " + two;
 	}
 }
